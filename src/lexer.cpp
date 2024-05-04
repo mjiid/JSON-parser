fc403526d5aed7e2a1826c6825c99349be4cc9a6
mjiid <habush1610@gmail.com>
2024-05-04 23:37:31 +0100

    add parseString(Ã() function

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -5,52 +5,69 @@
 
 class JSONLexer {
 public:
-	JSONLexer(const std::string& input) : input(input), position(0) {}
-	
-	std::vector<Token> tokenize() {
-		std::vector<Token> tokens;
-		
-		while (position < input.length()) {
-			char currentChar = input[position];
-			
-			// Skip whitespace
-			if (isspace(currentChar)) {
+    JSONLexer(const std::string& input) : input(input), position(0) {}
+
+    std::vector<Token> tokenize() {
+        std::vector<Token> tokens;
+
+        while (position < input.length()) {
+            char currentChar = input[position];
+
+            // Skip whitespace
+            if (isspace(currentChar)) {
                 position++;
                 continue;
             }
-            
+
             switch (currentChar) {
                 case '{':
                     tokens.push_back({TokenType::LeftBrace, "{"});
-              break;
+                    break;
                 case '}':
                     tokens.push_back({TokenType::RightBrace, "}"});
-              break;
+                    break;
                 case '[':
                     tokens.push_back({TokenType::LeftBracket, "["});
-              break;
-              case ']':
+                    break;
+                case ']':
                     tokens.push_back({TokenType::RightBracket, "]"});
-              break;
-              case ':':
+                    break;
+                case ':':
                     tokens.push_back({TokenType::Colon, ":"});
-              break;
-              case ',':
+                    break;
+                case ',':
                     tokens.push_back({TokenType::Comma, ","});
-              break;
-              case '"':
+                    break;
+                case '"':
                     tokens.push_back({TokenType::String, parseString()});
-              break;
-              default:
-                // Treat everything else as a separate token:
-                tokens.push_back({TokenType::None, std::string(1, currentChar)});
-                break;
+                    break;
+                default:
+                    // Treat everything else as a separate token:
+                    tokens.push_back({TokenType::None, std::string(1, currentChar)});
+                    break;
             }
 
             position++;
-		}
+        }
 
         return tokens;
-	}	
-}
+    }
+
+private:
+    std::string input;
+    size_t position;
+
+    std::string parseString() {
+        std::string stringValue;
+        position++; // Skip opening quote
+
+        while (position < input.length() && input[position] != '"') {
+            stringValue += input[position];
+            position++;
+        }
+
+        position++; // Skip closing quote
+        return stringValue;
+    }
+};
 
